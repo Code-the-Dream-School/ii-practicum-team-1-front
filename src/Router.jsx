f15e6376e7b70039103d3be01616ad142a1bc0f5
Маша <Maria@MacBook-Pro-Elena.local>
2025-03-28 21:18:47 +0300

    page and route structure updaed  - map and post edit pages and routes were deleted

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,10 +3,8 @@ import Landing from "./pages/Landing";
 import LoginAndRegister from "./pages/LoginRegister";
 import PostCreate from "./pages/PostCreate";
 import Post from "./pages/Post";
-import PostEdit from "./pages/PostEdit";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
-import Map from "./pages/Map";
 import AppLayout from "./layout/AppLayout";
 
 export default function AppRouter() {
@@ -15,13 +13,10 @@ export default function AppRouter() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<Landing />} />
-          <Route path="/map" element={<Map />} />
           <Route path="/post/new" element={<PostCreate />} />
           <Route path="/post/:id" element={<Post />} />
-          <Route path="/post/:id/edit" element={<PostEdit />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
-
         <Route path="/login" element={<LoginAndRegister />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/PostEdit.jsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function PostEdit() {
-  const { id } = useParams();
-  return <h1>Edit Post Page — ID: {id}</h1>;
-}
