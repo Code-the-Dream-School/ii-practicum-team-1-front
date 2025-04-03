4940c841bc6c35419882bd7fb6ed215638c48310
AidaBur <aidaburlutckaia@gmail.com>
2025-04-03 17:24:53 -0400

    Wrap App with AuthProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="895997461273-o3rstpf5bdr7scpo4kr85q0lq18lrvi4.apps.googleusercontent.com">
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 )
