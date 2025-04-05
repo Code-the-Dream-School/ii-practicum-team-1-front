811afc5b2ef4ba50b82660d1454a76def46b5402
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2025-04-05 14:29:40 -0700

    Added Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+// tailwind.config.js
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: [
+    "./src/**/*.{html, jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Custom extensions here
+      colors: {
+        'luxury-blue': '#1e3a8a',
+        'premium-indigo': '#3730a3'
+      }
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [
+    // Add plugins here
+  ],
+});
