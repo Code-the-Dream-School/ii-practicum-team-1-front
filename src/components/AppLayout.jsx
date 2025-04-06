0b7089e7c6b088cd64a0607215620113f421a2c9
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2025-04-05 20:38:32 -0700

    Added AppLayout

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import Sidebar from './Sidebar';
+import { Outlet } from 'react-router-dom';
 
-export default function AppLayout({ children }) {
+export default function MainLayout() {
   return (
-    <div>
-      {/* This is a place for Header */}
-      <main>
+    <div className="flex min-h-screen">
+      {/* Categories Sidebar */}
+      <div className="w-[26rem] sticky top-0 h-screen overflow-y-auto">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 overflow-y-auto">
         <Outlet />
-      </main>
-      {/* This is a place for Footer */}
+      </div>
     </div>
   );
 }
