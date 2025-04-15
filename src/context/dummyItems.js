ace57fc7abfebf82dc58b913af77b40f9094987c
AidaBur <aidaburlutckaia@gmail.com>
2025-04-15 09:51:16 -0400

    Temp: updated dummyItems with photos for testing UI

--- src/context/dummyItems.js
+++ src/context/dummyItems.js
@@ -1,56 +1,78 @@
 const dummyItems = [
-    {
-      item_id: 1,
-      title: "IKEA kids desk chair",
-      description: "Excellent condition.  And obviously you don’t have to put them together!!!   Cross posted",
-      category: "Furniture",
-      photos: [
-        "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0937014_pe793536_s5.jpg?f=xl", 
-        "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0937015_pe793537_s5.jpg?f=xl",
-        "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0612906_pe686092_s5.jpg?f=xl",
-        "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0841254_pe735808_s5.jpg?f=xl",
-        "https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      ],
-      status: "active",
-      location: "Miami, FL",
-    },
-    {
-      item_id: 2,
-      title: "Mountain Hiking",
-      description: "Explore breathtaking mountain trails.",
-      category: "Adventure",
-      photos: ["hiking1.jpg", "hiking2.jpg"],
-      status: "active",
-      location: "Aspen, CO",
-    },
-    {
-      item_id: 3,
-      title: "City Tour",
-      description: "Discover historical sites and modern attractions.",
-      category: "Urban",
-      photos: ["city1.jpg", "city2.jpg"],
-      status: "active",
-      location: "New York, NY",
-    },
-    {
-      item_id: 4,
-      title: "Safari Adventure",
-      description: "Experience wildlife in its natural habitat.",
-      category: "Wildlife",
-      photos: ["safari1.jpg", "safari2.jpg"],
-      status: "active",
-      location: "Kenya",
-    },
-    {
-      item_id: 5,
-      title: "Ski Trip",
-      description: "Hit the slopes with this exciting winter getaway.",
-      category: "Sports",
-      photos: ["ski1.jpg", "ski2.jpg"],
-      status: "active",
-      location: "Lake Tahoe, CA",
-    },
-  ];
-  
-  export default dummyItems;
-  
+  {
+    item_id: 1,
+    title: "IKEA kids desk chair",
+    description:
+      "Excellent condition.  And obviously you don’t have to put them together!!!   Cross posted",
+    category: "Furniture",
+    photos: [
+      "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0937014_pe793536_s5.jpg?f=xl",
+      "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0937015_pe793537_s5.jpg?f=xl",
+      "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0612906_pe686092_s5.jpg?f=xl",
+      "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0841254_pe735808_s5.jpg?f=xl",
+      "https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+    status: "active",
+    location: "Miami, FL",
+  },
+  {
+    item_id: 2,
+    title: "Mountain Hiking",
+    description: "Explore breathtaking mountain trails.",
+    category: "Furniture",
+    photos: [
+      "https://images.unsplash.com/photo-1566097127420-26750d93591e?q=80&w=3767&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1566097127353-0f282701b26a?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+    status: "active",
+    location: "Aspen, CO",
+  },
+  {
+    item_id: 3,
+    title: "City Tour",
+    description: "Discover historical sites and modern attractions.",
+    category: "Furniture",
+    photos: [
+      "https://images.unsplash.com/photo-1550226891-ef816aed4a98?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ],
+    status: "active",
+    location: "New York, NY",
+  },
+  {
+    item_id: 4,
+    title: "Safari Adventure",
+    description: "Experience wildlife in its natural habitat.",
+    category: "Furniture",
+    photos: ["https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc?q=80&w=3688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+    status: "active",
+    location: "Kenya",
+  },
+  {
+    item_id: 5,
+    title: "Ski Trip",
+    description: "Hit the slopes with this exciting winter getaway.",
+    category: "Furniture",
+    photos: ["https://images.unsplash.com/photo-1590675960591-37b658440031?q=80&w=2509&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+    status: "active",
+    location: "Lake Tahoe, CA",
+  },
+
+  {
+    item_id: 6,
+    title: "IKEA kids desk chair",
+    description:
+      "Excellent condition.  And obviously you don’t have to put them together!!!   Cross posted",
+    category: "Furniture",
+    photos: [
+      "https://images.unsplash.com/photo-1572734389279-e4fa423ca9db?q=80&w=3684&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0937015_pe793537_s5.jpg?f=xl",
+      "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0612906_pe686092_s5.jpg?f=xl",
+      "https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-skiftebo-dark-gray__0841254_pe735808_s5.jpg?f=xl",
+      "https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+    status: "active",
+    location: "Miami, FL",
+  },
+];
+
+export default dummyItems;
