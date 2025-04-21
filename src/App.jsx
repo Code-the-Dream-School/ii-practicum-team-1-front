025caf3a1af9b32a282dd2a8323ec9e980388b4f
AidaBur <aidaburlutckaia@gmail.com>
2025-04-21 10:58:21 -0400

    Clean up App.jsx: removed unused props since AppRouter handles post context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./components/Router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./context/AuthContext";
@@ -8,34 +8,15 @@ import { getAllData } from "./util/index";
 
 const URL = "http://localhost:8000/api/v1/";
 
-function AppContent() {
-  const location = useLocation();
-  const state = location.state;
-  const backgroundLocation = state && state.backgroundLocation;
-
-  return <AppRouter location={location} backgroundLocation={backgroundLocation} />;
-}
-
 function App() {
   const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      const myData = await getAllData(URL);
-      setMessage(myData.data);
-    })();
-
-    return () => {
-      console.log("unmounting");
-    };
-  }, []);
 
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <AuthProvider>
         <PostsProvider>
           <Router>
-            <AppContent />
+            <AppRouter />
           </Router>
         </PostsProvider>
       </AuthProvider>
