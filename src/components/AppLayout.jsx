91692faa5dae18f27a0de22507e38750452e65ad
AidaBur <aidaburlutckaia@gmail.com>
2025-05-02 11:25:47 -0400

    Removed Sidebar from AppLayout

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,17 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
 
 export default function AppLayout() {
   return (
-    <div className="w-full bg-white">
-      <div className="max-w-[1440px] mx-auto px-[100px] pt-20">
-        <div className="flex items-start">
-          <Sidebar />
-          <main className="flex-1">
-            <Outlet />
-          </main>
-        </div>
-      </div>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1">
+        <Outlet />
+      </main>
     </div>
   );
 }
