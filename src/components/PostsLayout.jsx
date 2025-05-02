bfef6c7c84cf976cf1b6b47a00b08080f211f261
AidaBur <aidaburlutckaia@gmail.com>
2025-05-02 12:05:08 -0400

    Show Sidebar only on /app/posts route (handles optional trailing slash)

--- src/components/PostsLayout.jsx
+++ src/components/PostsLayout.jsx
@@ -1,14 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 
 export default function PostsLayout() {
+  const location = useLocation();
+  const showSidebar = location.pathname.replace(/\/$/, "") === "/app/posts";
+
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <div className="flex flex-1">
-        <Sidebar />
-        <main className="flex-1 p-4 sm:p-6 md:p-8 overflow-y-auto w-full">
-          <Outlet />
-        </main>
+    <div className="w-full bg-white">
+      <div className="max-w-[1440px] mx-auto px-[100px] pt-20">
+        <div className="flex items-start">
+          {showSidebar && <Sidebar />}
+          <main className="flex-1">
+            <Outlet />
+          </main>
+        </div>
       </div>
     </div>
   );
