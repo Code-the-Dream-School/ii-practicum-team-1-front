c17e7fabc6947644ee57428c87da5e4ed33758e5
AidaBur <aidaburlutckaia@gmail.com>
2025-05-14 20:37:40 -0400

    move PostsProvider to wrap only posts-related routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./components/Router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./context/AuthContext";
-import { PostsProvider } from "./context/PostsContext";
-import { getAllData } from "./util/index";
+
 
 const URL = "http://localhost:8000/api/v1/";
 
@@ -14,11 +13,9 @@ function App() {
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <AuthProvider>
-        <PostsProvider>
           <Router>
             <AppRouter />
           </Router>
-        </PostsProvider>
       </AuthProvider>
     </GoogleOAuthProvider>
   );
