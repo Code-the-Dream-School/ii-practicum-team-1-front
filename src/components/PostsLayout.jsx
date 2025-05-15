436069493caf4148cedfcb579d47864e9fc71d47
AidaBur <aidaburlutckaia@gmail.com>
2025-05-14 20:37:53 -0400

    adjust PostsLayout to work with moved PostsProvider

--- src/components/PostsLayout.jsx
+++ src/components/PostsLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { PostsProvider } from "../context/PostsContext";
 
 export default function PostsLayout() {
   const location = useLocation();
   const showSidebar = location.pathname.replace(/\/$/, "") === "/app/posts";
 
   return (
+    <PostsProvider>
     <div className="w-full ">
       <div className="max-w-[1440px] mx-auto px-[20px] sm:px-[40px] lg:px-[100px] pt-10 ">
         <div className="flex items-start">
@@ -16,5 +18,6 @@ export default function PostsLayout() {
         </div>
       </div>
     </div>
+    </PostsProvider>
   );
 }
